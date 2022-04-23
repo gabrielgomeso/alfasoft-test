52de22b3f67639c7e487b2fe226e2e2adee1eaff
Gabriel Gomes <gabrielogomes1@gmail.com>
2022-04-23 10:35:37 -0300

    :wrench: Create base view for adding a contact

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/add">Create new contact</router-link>
   </nav>
   <router-view/>
 </template>
---
+++ src/views/AddContactView.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>This is an add page</h1>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>

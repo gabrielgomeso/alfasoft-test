62876edbfa64d734a3cebd80e35250509611bfb3
Gabriel Gomes <gabrielogomes1@gmail.com>
2022-04-23 11:38:13 -0300

    :sparkles: Add contact form base

--- src/views/AddContactView.vue
+++ src/views/AddContactView.vue
@@ -1,8 +1,43 @@
 <template>
   <h1>This is an add page</h1>
+
+  <form @submit.prevent="submitForm">
+    <label for="name">Contact name:</label>
+    <input v-model="form.contactName" type="text" name="name">
+
+    <label for="number">Contact number:</label>
+    <input v-model="form.contactNumber" type="text" name="number">
+
+    <label for="email">Contact e-mail:</label>
+    <input v-model="form.contactEmail" type="text" name="email">
+
+    <!-- <label for="photo">Contact photo:</label>
+    <input v-model="form.contactPhoto" type="file" name="photo"> -->
+
+    <button type="submit">Create Contact</button>
+  </form>
 </template>
 
 <script>
+import { createContact } from '../firebase.js';
+export default {
+  data() {
+    return {
+      form: {
+        contactName: '',
+        contactNumber: '',
+        contactEmail: '',
+        // contactPhoto: '',
+      },
+    }
+  },
+  methods: {
+    async submitForm() {
+      await createContact({ ...this.form });
+      console.log(this.form);
+    }
+  }
+}
 </script>
 
 <style>

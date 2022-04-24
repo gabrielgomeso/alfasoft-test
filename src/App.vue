d39c7cae6e4f51ce14dedefc58140bb4e6f9950f
Gabriel Gomes <gabrielogomes1@gmail.com>
2022-04-23 23:47:35 -0300

    :sparkle: Add TheHeader component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,26 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/add">Create new contact</router-link>
-  </nav>
+  <the-header></the-header>
   <router-view/>
 </template>
 
+<script>
+import TheHeader from "./components/TheHeader.vue";
+
+export default {
+  components: {
+    TheHeader,
+  }
+}
+
+</script>
+
 <style lang="scss">
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -15,16 +29,5 @@
   color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
--- src/components/TheForm.vue
+++ src/components/TheForm.vue
@@ -1,24 +1,55 @@
 <template>
   <form @submit.prevent="submitForm">
     <label for="name">Contact name:</label>
-    <input v-model="formData.contactName" type="text" name="name">
+    <input v-model="form.contactName" type="text" name="name">
 
     <label for="number">Contact number:</label>
-    <input v-model="formData.contactNumber" type="text" name="number">
+    <input v-model="form.contactNumber" type="text" name="number">
 
     <label for="email">Contact e-mail:</label>
-    <input v-model="formData.contactEmail" type="text" name="email">
+    <input v-model="form.contactEmail" type="text" name="email">
 
     <!-- <label for="photo">Contact photo:</label>
     <input v-model="form.contactPhoto" type="file" name="photo"> -->
 
-    <button type="submit">Create Contact</button>
+    <button @click="createNewContact" v-if="buttonType == 'newContact'" type="submit">
+      Create Contact
+    </button>
+
+    <button v-else @click="editContact" type="submit">
+      Edit Contact
+    </button>
   </form>
 </template>
 
 <script>
+  import { createContact, updateContact } from '../firebase.js';
+
   export default{
     props: ['form-data'],
+    data() {
+      return {
+        form: {
+          contactName: '',
+          contactNumber: '',
+          contactEmail: '',
+          // contactPhoto: '',
+        },
+      }
+    },
+    computed: {
+      buttonType() {
+        return this.$route.name;
+      }
+    },  
+    methods: {
+      async createNewContact() {
+        await createContact({ ...this.form });
+      },
+      async editContact() {
+        await updateContact({ ...this.form });
+      }
+    }
   }
 </script>
 
